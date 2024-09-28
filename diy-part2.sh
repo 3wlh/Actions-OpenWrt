2c8f1817817538851bed1c7e4fe3e6f29949c4cf
3wlh <1094890624@qq.com>
2024-09-28 11:50:28 +0800

    up

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,12 @@ sed -i 's/192.168.1.1/10.10.10.254/g' package/base-files/files/bin/config_genera
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+# 添加luci-theme-argon
+(git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon && {
+    [ -d package/luci-theme-argon ] && echo "CONFIG_PACKAGE_luci-theme-argon=y" >> .config 
+}) 
+# 添加luci-app-argon-config
+(git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config && {
+    [ -d package/luci-app-argon-config ] && echo "CONFIG_PACKAGE_luci-app-argon-config=y" >> .config 
+})
