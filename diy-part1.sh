3b79db9a229c406e2f9bc7e453f9a8494995b122
3wlh <1094890624@qq.com>
2024-09-28 08:11:57 +0800

    更新 diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,8 +18,8 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.defau
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon 
-(git clone https://github.com/3wlh/Packages package/3wlh-package && {
-    [ -d package/3wlh-package/luci-theme-argon ] && echo "CONFIG_PACKAGE_luci-theme-argon=y" >> .config
-    [ -d package/3wlh-package/luci-app-argon-config ] && echo "CONFIG_PACKAGE_luci-app-argon-config=y" >> .config
-    [ -d package/3wlh-package/luci-app-cifs-mount ] && echo "CONFIG_PACKAGE_luci-app-cifs-mount=y" >> .config
-})
+#(git clone https://github.com/3wlh/Packages package/3wlh-package && {
+#    [ -d package/3wlh-package/luci-theme-argon ] && echo "CONFIG_PACKAGE_luci-theme-argon=y" >> .config
+#    [ -d package/3wlh-package/luci-app-argon-config ] && echo "CONFIG_PACKAGE_luci-app-argon-config=y" >> .config
+#    [ -d package/3wlh-package/luci-app-cifs-mount ] && echo "CONFIG_PACKAGE_luci-app-cifs-mount=y" >> .config
+#})
