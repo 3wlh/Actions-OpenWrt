9c2af05c20af83fdbf020c6be48809f772741073
3wlh <1094890624@qq.com>
2024-10-05 08:51:16 +0800

    创建 diy.sh

---
+++ Configs/diy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#=================================================
+shopt -s extglob
+
+sed -i '/$(curdir)\/compile:/c\$(curdir)/compile: package/opkg/host/compile' package/Makefile
+sed -i "s/DEFAULT_PACKAGES:=/DEFAULT_PACKAGES:=luci-base luci-app-advancedplus luci-app-firewall luci-app-opkg luci-app-upnp \
+luci-app-wizard luci-compat luci-lib-ipkg luci-lib-fs \
+coremark wget-ssl curl autocore htop nano zram-swap kmod-lib-zstd kmod-tcp-bbr bash openssh-sftp-server block-mount resolveip ds-lite swconfig luci-app-fan luci-app-fileassistant /" include/target.mk
+
+sed -i "s/procd-ujail//" include/target.mk
+sed -i "s/procd-seccomp//" include/target.mk
+
+sed -i 's/Os/O2/g' include/target.mk
+sed -i "/mediaurlbase/d" package/feeds/*/luci-theme*/root/etc/uci-defaults/*
+sed -i 's/=bbr/=cubic/' package/kernel/linux/files/sysctl-tcp-bbr.conf
