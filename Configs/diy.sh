8a1f09966c64d9f067ec8f116bd9679ac9faea69
3wlh <1094890624@qq.com>
2024-10-23 15:03:54 +0800

    更新 diy.sh

--- Configs/diy.sh
+++ Configs/diy.sh
@@ -3,9 +3,9 @@
 shopt -s extglob
 
 sed -i '/$(curdir)\/compile:/c\$(curdir)/compile: package/opkg/host/compile' package/Makefile
-sed -i "s/DEFAULT_PACKAGES:=/DEFAULT_PACKAGES:=luci-base luci-app-advancedplus luci-app-firewall luci-app-opkg luci-app-upnp \
-luci-app-wizard luci-compat luci-lib-ipkg luci-lib-fs \
-coremark wget-ssl curl autocore htop nano zram-swap kmod-lib-zstd kmod-tcp-bbr bash openssh-sftp-server block-mount resolveip ds-lite swconfig luci-app-fan luci-app-fileassistant /" include/target.mk
+sed -i "s/DEFAULT_PACKAGES:=/DEFAULT_PACKAGES:=luci-base luci-app-firewall luci-app-opkg luci-app-upnp \
+luci-compat luci-lib-ipkg luci-lib-fs \
+coremark wget-ssl curl autocore htop nano zram-swap kmod-lib-zstd kmod-tcp-bbr bash openssh-sftp-server block-mount resolveip ds-lite swconfig luci-app-fan /" include/target.mk
 
 sed -i "s/procd-ujail//" include/target.mk
 sed -i "s/procd-seccomp//" include/target.mk
