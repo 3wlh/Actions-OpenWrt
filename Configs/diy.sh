642f392c1b3278245734abd9b3d1f6845ecf466e
3wlh <1094890624@qq.com>
2024-10-25 13:43:50 +0800

    更新 diy.sh

--- Configs/diy.sh
+++ Configs/diy.sh
@@ -1,28 +1,87 @@
 #!/bin/bash
 #=================================================
-shopt -s extglob
-
-sed -i '/$(curdir)\/compile:/c\$(curdir)/compile: package/opkg/host/compile' package/Makefile
-sed "s/DEFAULT_PACKAGES:=/DEFAULT_PACKAGES:=luci-base luci-app-firewall luci-app-opkg luci-app-upnp \
-luci-compat luci-lib-ipkg luci-lib-fs \
-coremark wget-ssl curl autocore htop nano zram-swap kmod-lib-zstd kmod-tcp-bbr bash openssh-sftp-server block-mount resolveip ds-lite swconfig luci-app-fan /" /root/target.mk
-
-sed -i "s/procd-ujail//" include/target.mk
-sed -i "s/procd-seccomp//" include/target.mk
-
-sed -i 's/Os/O2/g' include/target.mk
-sed -i "/mediaurlbase/d" package/feeds/*/luci-theme*/root/etc/uci-defaults/*
-sed -i 's/=bbr/=cubic/' package/kernel/linux/files/sysctl-tcp-bbr.conf
-
-luci
-
-
-sed '/DEFAULT_PACKAGES:=*/a\	luci \\' /root/target.mk
-
-
-DEFAULT_PACKAGES
-Default_Pakage=""
+# luci-base luci-app-firewall luci-app-opkg luci-app-upnp luci-compat luci-lib-ipkg luci-lib-fs coremark wget-ssl curl autocore htop nano zram-swap kmod-lib-zstd kmod-tcp-bbr bash openssh-sftp-server block-mount resolveip ds-lite swconfig luci-app-fan
+Pakages="DEFAULT_PACKAGES:=*"
+Source_branch="${1}"
+if [ -n "$(echo ${Source_branch} | grep "openwrt")" ]; then
+Default_Pakage="
+luci 
+luci-base
+luci-i18n-base-zh-cn
+luci-i18n-firewall-zh-cn
+luci-i18n-opkg-zh-cn  
+luci-lib-ipkg 
+luci-lib-fs 
+coremark 
+wget-ssl 
+curl 
+htop 
+nano 
+zram-swap 
+kmod-lib-zstd 
+kmod-tcp-bbr 
+bash 
+openssh-sftp-server 
+block-mount 
+resolveip 
+ds-lite 
+swconfig
+"
+fi
+if [ -n "$(echo ${Source_branch} | grep "lede")" ]; then
+Default_Pakage="
+luci 
+luci-base 
+autocore-arm 
+luci-i18n-base-zh-cn
+luci-i18n-firewall-zh-cn
+luci-i18n-opkg-zh-cn  
+luci-lib-ipkg 
+luci-lib-fs 
+coremark 
+wget-ssl 
+curl 
+htop 
+nano 
+zram-swap 
+kmod-lib-zstd 
+kmod-tcp-bbr 
+bash 
+openssh-sftp-server 
+block-mount 
+resolveip 
+ds-lite 
+swconfig
+"
+fi
+if [ -n "$(echo ${Source_branch} | grep "immortalwrt")" ]; then
+Default_Pakage="
+luci 
+luci-base
+luci-i18n-base-zh-cn
+luci-i18n-firewall-zh-cn
+luci-i18n-opkg-zh-cn  
+luci-lib-ipkg 
+luci-lib-fs 
+coremark 
+wget-ssl 
+curl 
+htop 
+nano 
+zram-swap 
+kmod-lib-zstd 
+kmod-tcp-bbr 
+bash 
+openssh-sftp-server 
+block-mount 
+resolveip 
+ds-lite 
+swconfig
+"
+fi
 for pakage in $(echo ${Default_Pakage} | sed 's/ / /')
 do
-	echo ${pakage}
+	# echo ${pakage}
+	sed -i '/'${Pakages}'/a\	'${pakage}' \\' include/target.mk
+	Pakages=${pakage}
 done
